5826bf741086f0bc44c728cfad6b950a4139cb14
akolle <andykoller55@yahoo.com>
2024-11-11 16:55:41 -0500

    nested routes

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link'
+
 function InfoPage() {
   return (
     <div>
       <h1 className="text-7xl">Info Page</h1>
+
+      <Link
+        href="/info/contact/"
+        className="text-xl text-blue-500 inline-block mt-8"
+      >
+        contact page
+      </Link>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,26 @@ import Link from 'next/link'
 
 function HomePage() {
   return (
-    <div>
+    <div className="bg-slate-300 p-3">
       <h1 className="text-7xl">Home Page</h1>
-      <Link href="/about" className="text-xl text-blue-500 inline-block mt-8">
-        about page
-      </Link>
+      <div className="uppercase">
+        <div>
+          <Link
+            href="/about"
+            className="text-xl text-blue-500 inline-block mt-8"
+          >
+            about page
+          </Link>
+        </div>
+        <div>
+          <Link
+            href="/info"
+            className="text-xl text-blue-500 inline-block mt-8"
+          >
+            info page
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
