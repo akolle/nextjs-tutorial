05cf6037ab538e111481909abc9c82db2b6ca230
akolle <andykoller55@yahoo.com>
2024-11-11 17:57:06 -0500

    navbar design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,11 +2,33 @@ import Link from 'next/link'
 
 function Navbar() {
   return (
-    <nav className="max-w-3xl mx-auto py-4 flex gap-x-4 border-b-2">
-      <Link href="/">Home</Link>
-      <Link href="/counter">Counter</Link>
-      <Link href="/tours">Tours</Link>
-      <Link href="/actions">Actions</Link>
+    <nav className=" border-b-2 bg-gradient-to-br from-slate-300 to-purple-500">
+      <div className="max-w-3xl mx-auto py-4 flex gap-x-4">
+        <Link
+          href="/"
+          className="transition duration-500 ease-in hover:bg-purple-900 hover:text-yellow-200"
+        >
+          Home
+        </Link>
+        <Link
+          href="/counter"
+          className="transition duration-500 ease-in hover:bg-purple-900 hover:text-yellow-200"
+        >
+          Counter
+        </Link>
+        <Link
+          href="/tours"
+          className="transition duration-500 ease-in hover:bg-purple-900 hover:text-yellow-200"
+        >
+          Tours
+        </Link>
+        <Link
+          href="/actions"
+          className="transition duration-500 ease-in hover:bg-purple-900 hover:text-yellow-200"
+        >
+          Actions
+        </Link>
+      </div>
     </nav>
   )
 }
