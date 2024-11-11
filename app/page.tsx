9bd411210705cab5a42946a1e684c4244bd9ae84
akolle <andykoller55@yahoo.com>
2024-11-11 17:27:06 -0500

    add navbar basic

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,6 @@ function HomePage() {
   return (
     <div className="bg-slate-300 p-3">
       <h1 className="text-7xl">Home Page</h1>
-      <div className="uppercase">
-        <div>
-          <Link
-            href="/about"
-            className="text-xl text-blue-500 inline-block mt-8"
-          >
-            about page
-          </Link>
-        </div>
-        <div>
-          <Link
-            href="/info"
-            className="text-xl text-blue-500 inline-block mt-8"
-          >
-            info page
-          </Link>
-        </div>
-      </div>
     </div>
   )
 }
