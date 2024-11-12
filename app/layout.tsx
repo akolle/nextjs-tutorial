7c075eb29f128590943d739161f17a8b3d961f44
akolle <andykoller55@yahoo.com>
2024-11-11 19:46:03 -0500

    add metadata from api

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter, Inconsolata, Roboto } from 'next/font/google'
+import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['cyrillic'] })
 const incon = Inconsolata({ subsets: ['latin'] })
 const roboto = Roboto({ subsets: ['latin'], weight: ['400'] })
+
+export const metadata: Metadata = {
+  title: 'Next.js Project',
+  description: 'A Next.js project with TypeScript and TailwindCSS.',
+  keywords: 'Next.js, Typescript, TailwindCSS',
+}
 
 export default function RootLayout({
   children,
