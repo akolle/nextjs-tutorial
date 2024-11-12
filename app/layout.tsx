7efd75e83944d7ab0e237475bc48a4fb893cd03d
akolle <andykoller55@yahoo.com>
2024-11-11 19:38:10 -0500

    fonts and update to css to green navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
-import Navbar from '../components/Navbar'
+import Navbar from '@/components/Navbar'
 import './globals.css'
+import { Inter, Inconsolata, Roboto } from 'next/font/google'
+
+const inter = Inter({ subsets: ['cyrillic'] })
+const incon = Inconsolata({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin'], weight: ['400'] })
 
 export default function RootLayout({
   children,
@@ -8,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <Navbar />
         <main className="max-w-3xl mx-auto py-4">{children}</main>
       </body>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,29 +2,29 @@ import Link from 'next/link'
 
 function Navbar() {
   return (
-    <nav className=" border-b-2 bg-gradient-to-br from-slate-300 to-purple-500">
+    <nav className=" border-b-2 bg-gradient-to-br from-slate-300 to-green-900">
       <div className="max-w-3xl mx-auto py-4 flex gap-x-4">
         <Link
           href="/"
-          className="transition duration-500 ease-in hover:bg-purple-900 hover:text-yellow-200"
+          className="transition duration-500 ease-in hover:bg-green-900 hover:text-yellow-200"
         >
           Home
         </Link>
         <Link
           href="/counter"
-          className="transition duration-500 ease-in hover:bg-purple-900 hover:text-yellow-200"
+          className="transition duration-500 ease-in hover:bg-green-900 hover:text-yellow-200"
         >
           Counter
         </Link>
         <Link
           href="/tours"
-          className="transition duration-500 ease-in hover:bg-purple-900 hover:text-yellow-200"
+          className="transition duration-500 ease-in hover:bg-green-900 hover:text-yellow-200"
         >
           Tours
         </Link>
         <Link
           href="/actions"
-          className="transition duration-500 ease-in hover:bg-purple-900 hover:text-yellow-200"
+          className="transition duration-500 ease-in hover:bg-green-900 hover:text-yellow-200"
         >
           Actions
         </Link>
